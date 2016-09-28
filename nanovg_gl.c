f5e1b0273d1c610a5becde920164440ff8c925b6
Jeff R <ruhlio@users.noreply.github.com>
2016-09-27 22:50:02 -0500

    Fixed nanovg submodule

---
+++ nanovg_gl.c
@@ -0,0 +1,7 @@
+#include <sys/types.h>
+#include <GL/gl.h>
+
+#include "./nanovg/src/nanovg.h"
+
+#define NANOVG_GL3_IMPLEMENTATION
+#include "./nanovg/src/nanovg_gl.h"
--- nanovg_shim.h
+++ nanovg_shim.h
@@ -1,4 +1,11 @@
+#include <sys/types.h>
+#include <GL/gl.h>
+
 #include "./nanovg/src/nanovg.h"
 
-#define NANOVG_GL2_IMPLEMENTATION
-#include "./nanovg/src/nanovg_gl.h"
+// directly define prototypes to prevent linkage errors with static functions
+NVGcontext* nvgCreateGL3(int flags);
+void nvgDeleteGL3(NVGcontext* ctx);
+
+int nvglCreateImageFromHandleGL3(NVGcontext* ctx, GLuint textureId, int w, int h, int flags);
+GLuint nvglImageHandleGL3(NVGcontext* ctx, int image);
